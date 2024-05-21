d381a293a1e02caa165998a7ce4c4e6351ccfe6c
Aniebietabasi <aniebietabasiobo@gmail.com>
2024-05-21 13:35:30 +0100

    commit

--- src/components/home/Blog.tsx
+++ src/components/home/Blog.tsx
@@ -6,7 +6,7 @@ function Blog() {
   return (
     <div className="p-[50px_20px] lg:p-[80px_80px] flex items-center justify-center">
       <div className="grid gap-[40px] lg:grid-cols-3  2xl:container">
-        <div className=" p-[20px_10px] h-[450px] flex flex-col items-start justify-between">
+        <div className=" p-[20px_10px] h-[430px] flex flex-col items-start justify-between">
           <div>
             <div className="w-fit flex items-center dark:bg-[#242627] bg-green-50 p-[5px_10px] space-x-[5px] rounded-[5px]">
               <TiStarburst className="text-primary" />
@@ -28,8 +28,22 @@ function Blog() {
           </Link>
         </div>
         {Array.from({ length: 2 }).map((_, index) => (
-          <div className="h-[450px] border rounded-[10px]" key={index}>
-            <div className="h-[250px] w-full border-b"></div>
+          <div className="h-[430px] border rounded-[10px] flex flex-col" key={index}>
+            <div className="h-[250px] w-full border-b relative overflow-hidden rounded-t-[10px]">
+                <div className="absolute"></div>
+                <div className="bg-green-100 h-full w-full"></div>
+            </div>
+            <div className="p-[20px] flex flex-col grow items-start justify-between">
+                <div className="flex items-center space-x-[10px]">
+                    <p className="text-[13px] underline font-inter">Cyber Security</p>
+                    <p className="text-[13px] font-inter">Comments (16)</p>
+                </div>
+                <div className="text-[22px] font-inter font-[500]">How to Been population the Startup Company of this.</div>
+            <Link href="/blog" className="underline flex items-center font-inter">
+            <p>Read More</p>
+            <GoArrowUpRight />
+          </Link>
+            </div>
           </div>
         ))}
       </div>
