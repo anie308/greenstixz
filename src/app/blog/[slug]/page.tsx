a024eafc911f9405de4d762afc6a4e17c82a35a3
Aniebietabasi <aniebietabasiobo@gmail.com>
2024-05-25 01:12:06 +0100

    commit

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -2,22 +2,44 @@
 
 import { getEntryBySlug } from "@/service";
 import React, { useEffect } from "react";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import NewsLetter from "@/components/NewsLetter";
+import Image from "next/image";
 
 function SinglePost({ params }: { params: { slug: string } }) {
+  const [post, setPost] = React.useState<any>({});
   useEffect(() => {
     const getSinglePost = async () => {
       try {
         const post = await getEntryBySlug(params.slug, "blogPost");
-        console.log(post);
+        // console.log(post);
+        setPost(post);
       } catch (error) {
         console.log(error);
       }
     };
     getSinglePost();
-    console.log("params", params);
-  });
+  }, [params.slug]);
 
-  return <div>{params?.slug}</div>;
+  return (
+    <div className="p-[50px_20px] lg:p-[80px_80px] flex flex-col items-center justify-center">
+      {post ? (
+        <div className="w-full lg:w-[70%]">
+          <h1 className="text-[30px] font-inter font-[700]">
+            {post?.fields?.title}
+          </h1>
+          <div className="h-[300px] lg:h-[400px] w-full border-2 mt-[20px] overflow-hidden">
+            <img src={`https:${post?.fields?.featuredImage?.fields?.file?.url}`} className="h-full w-full object-cover" alt=""  />
+          </div>
+
+          <div className="mt-[40px] font-inter text-[16px] lg:text-[18px] leading-relaxed">
+            {documentToReactComponents(post?.fields?.content)}
+          </div>
+        </div>
+      ) : null}
+      <NewsLetter/>
+    </div>
+  );
 }
 
 export default SinglePost;
